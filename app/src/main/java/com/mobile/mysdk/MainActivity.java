c737ab4b42f416bdb1a6b453c944c4e5c675b8b6
Dev <congminh.le@gosu.vn>
2021-04-07 15:13:35 +0700

    up inap billing 3.0.0

--- app/src/main/java/com/mobile/mysdk/MainActivity.java
+++ app/src/main/java/com/mobile/mysdk/MainActivity.java
@@ -287,7 +287,7 @@ public class MainActivity extends Activity {
         }
     }
 
-   
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         mGamo.onActivityResult(requestCode, resultCode, data);
